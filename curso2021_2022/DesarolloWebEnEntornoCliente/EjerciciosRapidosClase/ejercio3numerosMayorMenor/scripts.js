dd029633162846c021c1171a75e411da742c262d
Luis <luisaguadodiego1993@gmail.com>
2024-10-07 12:55:12 +0200

    1.0

---
+++ curso2021_2022/DesarolloWebEnEntornoCliente/EjerciciosRapidosClase/ejercio3numerosMayorMenor/scripts.js
@@ -0,0 +1,19 @@
+//ordenar tres variables
+
+let a =3;
+let b=2;
+let c=1;
+
+let x=Math.min(a,b,c);
+let y=Math.max(a,b,c);
+let z=(a+b+c)-x-y;
+
+
+//x=1
+//y=3
+// z=(6)-1 -3
+//z=2
+
+alert(x);
+alert(z);
+alert(y);
